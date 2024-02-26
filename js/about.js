4dde6eeb474c1146be1ea29e804ec2f256112d3e
zahid-191 <161313072+zahid-191@users.noreply.github.com>
2024-02-26 18:12:25 +0600

    Add files via upload

---
+++ js/about.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+            delay: 10,
+            time: 5000
+        });
+});
+
+//  animation
+
+AOS.init({
+    duration:2000
+});
